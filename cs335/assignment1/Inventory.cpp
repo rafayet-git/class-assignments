f177331b0fe96712fd545a6c8286accb1fe5142d
rafayet <78610949+rafayet-git@users.noreply.github.com>
2025-02-07 11:57:42 -0500

    Add some funcs

--- cs335/assignment1/Inventory.cpp
+++ cs335/assignment1/Inventory.cpp
@@ -1,15 +1,23 @@
 #include "Inventory.hpp"
 
-Inventory::Inventory(const std::vector<std::vector<Item>>& items, Item* equipped){
-
+Inventory::Inventory(const std::vector<std::vector<Item>>& items, Item* equipped) : 
+    inventory_grid_(items), equipped_(equipped), weight_(0), item_count_(0) {
+    for (const auto& row : items){
+        for (const auto& item : row){
+            if (item.type_ != NONE){
+                weight_ += item.weight_;
+                item_count_++;
+            }
+        }
+    }
 }
 
 Item* Inventory::getEquipped() const{
-
+    return equipped_;
 }
 
 void Inventory::equip(Item* itemToEquip){
-
+    equipped_ = itemToEquip;
 }
 
 void Inventory::discardEquipped(){
@@ -56,6 +64,6 @@ Inventory& Inventory::operator=(Inventory&& rhs){
 }
 
 Inventory::~Inventory(){
-
+    delete equipped_;
 }
 
