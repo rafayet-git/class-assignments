a5b2c10d004eadfbb498d8b3ae8072b7fd724e54
rafayet <78610949+rafayet-git@users.noreply.github.com>
2025-03-23 00:41:59 -0400

    Input check

--- cs260/proj1.asm
+++ cs260/proj1.asm
@@ -1,25 +1,42 @@
 .data
 # DONOTMODIFYTHISLINE
-frameBuffer: 	.space 0x80000 # 512 wide X 256 high pixels
-M:		.word 15
-N:		.word 10
-cr:		.word 32
-cg:		.word 64
-cb:		.word 128
+frameBuffer: 	.space 0x80000	# 512 wide X 256 high pixels
+M:		.word  100
+N:		.word  76
+cr:		.word  10
+cg:		.word  64
+cb:		.word  10
 # DONOTMODIFYTHISLINE
 # Your other variables go BELOW here only
 .text
-drawLine: la $t1,frameBuffer
-li $t3,0x0000FF00 # $t3 ← green
-sw $t3,15340($t1)
-sw $t3,15344($t1)
-sw $t3,15348($t1)
-sw $t3,15352($t1)
-sw $t3,15356($t1)
-sw $t3,15360($t1)
-sw $t3,15364($t1)
-sw $t3,15368($t1)
-sw $t3,15372($t1)
-sw $t3,15376($t1)
-li $v0,10 # exit code
-syscall # exit to OS
+makeBG: 	la   $t1,frameBuffer	# t1 <- addr of bitmap start
+		addi $t2,$zero,512	# t2 <- 512
+		sll  $t2,$t2,10		# t2 <- 512 * 256 * 4
+		add  $t2,$t1,$t2 	# t2 <- addr of bitmap end
+		li   $t3,0x00FFFF00	# t3 <- yellow
+loopBG:		sw   $t3,0($t1)		# bitmap pixel <- yellow
+		addi $t1,$t1,4		# t1 <- next pixel on bitmap
+		beq  $t1,$t2,checkInput	# exit loopBG if t1 == end of bitmap 
+		j    loopBG		# iterate
+		
+		# Check M and N if they are within range
+checkInput:	la   $t1,M		# t1 <- addr of M
+		lw   $t2,0($t1)		# t2 <- M
+		lw   $t3,4($t1)		# t3 <- N
+		slti $t4,$t2,256	# t4 <- 1 if M < 256 (height), else t4 <- 0
+		beq  $t4,$zero,endCode	# exit code if invalid input
+		slt  $t4,$t3,$t2	# t4 <- 1 if N < M, else t4 <- 0
+		beq  $t4,$zero,endCode	# exit code if invalid input
+		slt  $t4,$zero,$t3	# t4 <- 1 if 0 < N, else t4 <- 0
+		beq  $t4,$zero,endCode	# exit code if invalid input
+		slt  $t4,$zero,$t2	# t4 <- 1 if 0 < M, else t4 <- 0
+		beq  $t4,$zero,endCode	# exit code if invalid input
+		# Check if centerable by checking bit 0 for even value.
+		andi $t4,$t2,0x0001	# t4 <- 0 if M is even, else t4 <- 1
+		bne  $t4,$zero,endCode	# exit code if invalid input
+		andi $t4,$t3,0x0001	# t4 <- 0 if N is even, else t4 <- 1
+		bne  $t4,$zero,endCode	# exit code if invalid input
+calcRGB:
+
+endCode:	li $v0,10	# exit code
+		syscall 	# exit to OS
