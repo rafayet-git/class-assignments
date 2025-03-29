89c4871f9552bc241052bd3fb66cffe018544085
rafayet <78610949+rafayet-git@users.noreply.github.com>
2025-03-29 16:57:51 -0400

    Update

--- cs260/proj1.asm
+++ cs260/proj1.asm
@@ -20,7 +20,7 @@ makeBG: 	add  $t0,$zero,$s0	# t0 <- bitmap start
 		addi $t1,$zero,512	# t1 <- 512
 		sll  $t1,$t1,10		# t1 <- 512 * 256 * 4
 		add  $t1,$t0,$t1 	# t1 <- addr of bitmap end
-		li   $t2,0x00FFFF00	# t2 <- yellow
+		li   $t2,0x00feeb3d	# t2 <- yellow
 loopBG:		sw   $t2,0($t0)		# bitmap pixel <- yellow
 		addi $t0,$t0,4		# t0 <- next pixel on bitmap
 		beq  $t0,$t1,checkInput	# exit loopBG if t0 == end of bitmap 
@@ -55,13 +55,84 @@ loopRGB:	lw   $t2,0($t0)		# t2 <- crgb value
 		slti $t4,$t2,256	# t4 <- 1 if crgb*4 < 256, else t4 <- 0
 		beq  $t4,$zero,setRGB 	# branch if invalid color 
 retRGB:		add  $t8,$t8,$t2	# t8 <- arrow color (-,-,crgb*4)
-		beq  $t0,$t1,draw	# exit loop if colors are fully set
+		beq  $t0,$t1,center	# exit loop if colors are fully set
 		addi $t0,$t0,4		# t1 <- addr of next crgb value
 		j    loopRGB		# iterate
 setRGB:		addi $t2,$zero,255	# t2 <- largest crgb value
 		j    retRGB		# return to loop
 
-draw:		
+center:		addi $t1,$zero,256	# t1 <- 256
+		addi $t2,$s1,8		# t2 <- M+8
+		sub  $t0,$t1,$t2	# t0 <- 256-(M+8)
+		sll  $t0,$t0,10		# t0 <- bitmap row for centering arrow: (256-(M+8))/2 * 512
+		addi $t0,$t0,-2048	# t0 <- bitmap row-1
+		sub  $t3,$s1,$s2	# t3 <- M-N
+		srl  $t1,$t3,1		# t1 <- (M-N)/2
+		add  $t1,$t1,$t2	# t1 <- (M-N)/2 + M+8
+		addi $t2,$zero,512      # t2 <- 512
+		sub  $t1,$t2,$t1	# t1 <- 512 - ((M-N)/2 + M+8)
+		sll  $t1,$t1,1		# t1 <- bitmap column for centering arrow: (512 - ((M-N)/2 + M+8))/2 * 4
+		add  $t1,$t1,-4		# t1 <- bitman column - 1
+		add  $t0,$s0,$t0	# t0 <- bitmap[row][0]
+		add  $t0,$t1,$t0	# t0 <- bitmap[row][col]
+		
+drawVBox:	add  $t1,$zero,$zero	# t1 <- 0
+		add  $t4,$t0,$zero	# t4 <- bitmap[row][col]
+loopDVBOut:	slt  $t5,$t1,$t3	# t5 <- 1 if t1 < M-N, else t5 <- 0
+		beq  $t5,$zero,drawHBox	# exit loop if part fully drawn
+		add  $t2,$zero,$zero	# t2 <- 0
+loopDVBIn:	slt  $t5,$t2,$s1	# t5 <- 1 if t2 < M, else t5 <- 0
+		beq  $t5,$zero,endDVBIn	# exit loop if line drawn
+		sw   $t8,0($t4)		# bitmap pixel <- arrow color
+		addi $t4,$t4,2048	# t4 <- bitmap[row+1][col]
+		addi $t2,$t2,1		# t2 <- t2 +1
+		j    loopDVBIn		# iterate
+endDVBIn:	addi $t0,$t0,4		# t0 <- bitmap[row][col+1]
+		add  $t4,$t0,$zero	# t4 <- bitmap[row][col+1]
+		addi $t1,$t1,1		# t1 <- t1+1
+		j    loopDVBOut		# iterate
 
+drawHBox:	sll  $t1,$s2,11		# t1 <- N*4*512
+		add  $t0,$t0,$t1	# t0 <- bitmap[row+N][col]
+		add  $t4,$t0,$zero	# t4 <- bitmap[row+N][col]
+		add  $t1,$zero,$zero	# t1 <- 0
+loopDHBOut:	slt  $t5,$t1,$s2	# t5 <- 1 if t1 < N, else t5 <- 0
+		beq  $t5,$zero,drawHead	# exit loop if part fully drawn
+		add  $t2,$zero,$zero	# t2 <- 0
+loopDHBIn:	slt  $t5,$t2,$t3	# t5 <- 1 if t2 < (M-N), else t5 <- 0
+		beq  $t5,$zero,endDHBIn	# exit loop if line drawn
+		sw   $t8,0($t4)		# bitmap pixel <- arrow color
+		addi $t4,$t4,2048	# t4 <- bitmap[row+1][col]
+		addi $t2,$t2,1		# t2 <- t2 +1
+		j    loopDHBIn		# iterate
+endDHBIn:	addi $t0,$t0,4		# t0 <- bitmap[row][col+1]
+		add  $t4,$t0,$zero	# t4 <- bitmap[row][col+1]
+		addi $t1,$t1,1		# t1 <- t1+1
+		j    loopDHBOut		# iterate
+
+#outer loop:
+#if t3=0 exit loop
+#	inner loop:
+# 	if i<t3 false, exit loop
+#	sw t4
+#	t4 + 2048
+# 	i++
+# 	repeat
+# t3 -= 2
+# t0 + 2052
+# t4 = t0
+# repeat
+drawAHead:	addi $t0,$t0,-16384	# t0 <- bitmap[row-8][col]
+		add  $t4,$t0,$zero	# t4 <- bitmap[row-8][col]
+		addi $t3,$t3,16		# t3 <- (M-N)+16
+		
+loopDAHOut:
+loopDAHIn:
+endDAHIn:
+		sw   $t9,0($t4)
+		sw   $t9,4($t4)
+		sw   $t9,8($t4)
+		sw   $t9,12($t4)
+		
 endCode:	li   $v0,10	# exit code
 		syscall 	# exit to OS
