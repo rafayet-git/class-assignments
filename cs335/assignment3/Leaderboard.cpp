9b7e71602ae6912399fb86756da25a7d11772434
rafayet <78610949+rafayet-git@users.noreply.github.com>
2025-04-29 11:39:26 -0400

    do

--- cs335/assignment3/Leaderboard.cpp
+++ cs335/assignment3/Leaderboard.cpp
@@ -50,3 +50,33 @@ RankingResult Offline::heapRank(std::vector<Player>& players){
   return RankingResult(std::vector<Player>(players.end()-tops,players.end()), {}, duration.count());
   
 }
+
+/**
+ * @brief Uses a mixture of quickselect/quicksort to
+ *        select and sort the top 10% of players with O(log N) memory
+ *        (excluding the returned RankingResult vector)
+ *
+ * @param players A reference to the vector of Player objects to be ranked
+ * @return A Ranking Result object whose
+ * - top_ vector -> Contains the top 10% of players from the input in sorted order (ascending)
+ * - cutoffs_    -> Is empty
+ * - elapsed_    -> Contains the duration (ms) of the selection/sorting operation
+ *
+ * @post The order of the parameter vector is modified.
+ */
+RankingResult Offline::quickSelectRank(std::vector<Player>& players){
+  if (players.empty())
+    return RankingResult({},{},0);
+
+  int tops = std::floor(0.1*players.size());
+  if (tops == 0) tops++;
+  tops = players.size() - tops; // quickselect to find position tops
+  
+  auto start_time = std::chrono::high_resolution_clock::now(); // timer start
+  std::nth_element(players.begin(), players.begin()+tops, players.end());
+  //...
+  auto end_time = std::chrono::high_resolution_clock::now(); // timer end
+  std::chrono::duration<double, std::milli> duration = end_time - start_time;
+
+  return RankingResult(std::vector<Player>(players.begin()+tops,players.end()), {}, duration.count());
+}
